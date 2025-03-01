d8faa5bc0512723139a351ba79b18a7029fad092
swayam patial <swayampatial1@gmail.com>
2025-03-02 01:15:01 +0530

    third commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,13 @@
+import { Button } from "@/components/ui/button";
+
 function HomePage() {
   return (
-    <div>HomePage</div>
-  )
+    <div>
+      <h1 className="text-3xl">HomePage</h1>
+      <Button variant="outline" size="lg" className="capitalize m-8">
+        click
+      </Button>
+    </div>
+  );
 }
-export default HomePage
+export default HomePage;
