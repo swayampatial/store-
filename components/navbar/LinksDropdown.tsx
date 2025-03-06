3a8dab6627ffb5c9fb9f839dc62d1ee1dffd3a6d
swayam patial <swayampatial1@gmail.com>
2025-03-07 02:03:04 +0530

    fifth commit

--- components/navbar/LinksDropdown.tsx
+++ components/navbar/LinksDropdown.tsx
@@ -1,6 +1,34 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import { LuAlignLeft } from 'react-icons/lu';
+import Link from 'next/link';
+import { Button } from '../ui/button';
+import { links } from '@/utils/links';
+
 function LinksDropdown() {
   return (
-    <div>LinksDropdown</div>
-  )
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant='outline' className='flex gap-4 max-w-[100px]'>
+          <LuAlignLeft className='w-6 h-6' />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className='w-40' align='start' sideOffset={10}>
+        {links.map((link) => {
+          return (
+            <DropdownMenuItem key={link.href}>
+              <Link href={link.href} className='capitalize w-full'>
+                {link.label}
+              </Link>
+            </DropdownMenuItem>
+          );
+        })}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
 }
-export default LinksDropdown
+export default LinksDropdown;
--- components/navbar/Logo.tsx
+++ components/navbar/Logo.tsx
@@ -12,3 +12,4 @@ function Logo() {
   );
 }
 export default Logo;
+  
